775a80b97ab18c1491f44fcee753f964462322b5
Apprentice <apprentice@devbootcamp.com>
2013-04-03 16:01:57 -0700

    Can now add questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,4 +7,15 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
     @answers = Answer.where('question_id = ?', @question.id)
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    params[:question][:user_id] = 1 #TODO: Hardcoded
+    question = Question.create!(params[:question])
+
+    redirect_to question_path(question)
+  end
 end
