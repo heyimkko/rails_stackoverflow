6fd1e6e58fa4ca1572e22436f023f9ec177d8442
Apprentice <apprentice@devbootcamp.com>
2013-04-03 17:15:52 -0700

    USER AUTH COMPLETED YEEHAW

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,16 @@
 class SessionsController < ApplicationController
 
   def new 
-    @user = User.new
   end
 
   def create
-    @user = User.find_by_email(params[:user][:email].downcase)
-    if @user && @user.authenticate(params[:user][:password])
+    @user = User.find_by_email(params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
       login(@user)
       redirect_to root_path
     else
       render :new
-    end
+    end  
   end
 
   def destroy
